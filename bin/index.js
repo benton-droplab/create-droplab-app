fafaf061ab0fcca976622dc6b0fd0c24a1f98694
Mike D <miked@droplab.com>
2022-04-20 11:20:40 -0700

    Improved UI with spinners and async functions

--- bin/index.js
+++ bin/index.js
@@ -1,29 +1,142 @@
 #! /usr/bin/env node
 
 const path = (await import('path')).default;
+const replace = (await import('replace-in-file')).default;
 const chalk = (await import('chalk')).default;
 const shell = (await import('shelljs')).default;
+const _ = (await import('lodash')).default;
+const spinners = (await import('cli-spinners')).default;
+const ora = (await import('ora')).default;
 // const inquirer = (await import('inquirer')).default;
 // const figlet = (await import('figlet')).default;
 
+const args = process.argv.slice(2);
+const log = console.log;
+
+
+
+shell.echo(`\n`);
+
+const spinner = ora({
+	symbol: '🚀',
+	text: `Starting...`,
+	discardStdin: false
+}).start();
+
+
+
 if(!shell.which('git')) 
 {
-	shell.echo(chalk.red(`\nSorry, this script requires ${chalk.underline(`git`)}. Please install before proceeding.\n`));
+	spinner.stopAndPersist({
+		symbol: '💀',
+		text: `${chalk.red(`Sorry, this script requires ${chalk.underline.bold(`git`)}. Please install before proceeding -> ${chalk.underline(`https://github.com/git-guides/install-git`)}`)}`
+	});
+	
+	shell.echo(`\n`);
 	shell.exit(1);
 }
 
 if(!shell.which('vercel')) 
 {
-	shell.echo(chalk.red(`\nSorry, this script requires the ${chalk.underline(`vercel cli`)}. Please install before proceeding via `) + chalk.bgRed.white(`npm i -g vercel`)+`\n`);
+	spinner.stopAndPersist({
+		symbol: '💀',
+		text: `${chalk.red(`Sorry, this script requires ${chalk.underline.bold(`vercel cli`)}. Please install before proceeding via `) + chalk.bgRed.white(`npm i -g vercel`)}`
+	});
+	
+	shell.echo(`\n`);
 	shell.exit(1);
 }
 
-const log = console.log;
 
-const args = process.argv.slice(2);
 
 let templateUrl = 'https://github.com/droplab/droplab-site-templates.git';
 let destFolder = '.';
+let projectName = 'my-project';
+
+const cloneRepo = (dir,repo) => 
+{
+	return new Promise((resolve,reject) => 
+	{
+		shell.exec(`git clone ${repo} ${dir} -q`,
+			(code, stdout, stderr) =>
+			{
+				if(code !== 0) 
+				{
+					reject(new Error('Git clone failed'));
+					return;
+				}
+
+				resolve({ code,stdout,stderr });
+			}
+		);
+	})
+	.catch(err => 
+	{
+		shell.echo(`Error: ${err.message}`);
+		shell.exit(1);
+	});
+}
+
+const cloneEnvFile = (templateFile,outputFile,projectName) => 
+{
+	return new Promise((resolve,reject) => 
+	{
+		replace(
+			{
+				files: templateFile,
+				from: '%%PROJECT-NAME%%',
+				to: projectName,
+			}, 
+			(error,results) => 
+			{
+				setTimeout(() =>
+				{
+					if(error) 
+					{
+						reject(new Error(error));
+						return;
+					}
+
+					shell.cp(templateFile,outputFile);
+					shell.rm(`-f`,templateFile);
+
+					resolve({ results });
+
+				},3000);
+			}
+		);
+	})
+	.catch(err => 
+	{
+		shell.echo(`Error: ${err.message}`);
+		shell.exit(1);
+	});
+}
+
+const installDependencies = () => 
+{
+	return new Promise((resolve,reject) => 
+	{
+		shell.exec(`cd ${destFolder} && yarn install`, { silent:true },
+			(code, stdout, stderr) =>
+			{
+				if(code !== 0) 
+				{
+					reject(new Error('Error occured while installing dependencies'))
+					return
+				}
+
+				resolve({ code,stdout,stderr })
+			}
+		);
+	})
+	.catch(err => 
+	{
+		shell.echo(`Error: ${err.message}`)
+		shell.exit(1)
+	});
+}
+
 
 switch(args.length)
 {
@@ -58,43 +171,69 @@ switch(args.length)
 	break;
 }
 
+projectName = _.kebabCase(path.basename(path.resolve(destFolder)));
+
+
+
 try 
 {
-	shell.echo(`\n${chalk.green(`Scaffolding project from ${chalk.blue.bold(templateUrl)} into ${chalk.bold(destFolder)}...`)}`);
-	
-	const { code,stdout,stderr } = shell.exec(`git clone ${templateUrl} ${destFolder}`, { silent:true });
+	spinner.color = 'green';
+	spinner.text = `Scaffolding project from ${chalk.cyan.bold(templateUrl)} into ${chalk.magenta.bold(destFolder)}...`;
+
+	const { code,stdout,stderr} = await cloneRepo(destFolder,templateUrl);
 
 	if(code === 0)
 	{
 		shell.rm(`-rf`,`${destFolder}/.git`);
 		shell.rm(`-rf`,`${destFolder}/.github`);
-		
-		const { code2,stdout2,stderr2 } = shell.exec(`vercel env pull development ${destFolder}/.env.local`, { silent:true });
 
-		if(code2 !== 0)
-		{
-			shell.echo(`\n${chalk.yellow(`Unable to pull vercel environment vars... creating clean ${chalk.bold(`.env.local`)} file`)}`);
+		// Setup env vars file
+		//
+			spinner.color = 'yellow';
+			spinner.text = `${chalk.yellow(`Setting up ${chalk.bold(`.env.local`)} file...`)}`;
+
+			await cloneEnvFile(
+				`${destFolder}/env.local.temp`,
+				`${destFolder}/.env.local`,
+				projectName
+			);
 			
-			shell.cp(`${destFolder}/env.local.temp`,`${destFolder}/.env.local`);
-		}
-		else
-		{
-			shell.echo(`\n${chalk.green(`Synchronized vercel environment vars to ${chalk.bold(`.env.local`)}`)}`);
-		}
+		// Install dependencies
+		//
+			spinner.color = 'cyan';
+			spinner.text = `${chalk.cyan(`Installing dependencies...`)}`;
 
-		shell.rm(`-f`,`${destFolder}/env.local.temp`);
-		
-		shell.echo(`\n${chalk.green(`[SUCCESS]`)}\n`);
+			const { code3,stdout3,stderr3 } = await installDependencies();
+
+		spinner.stopAndPersist({
+			symbol: '✨',
+			text: `${chalk.bold(chalk.green(`Done! Let's Fucking Go! 🚀🚀`))}`
+		})
+	
+		shell.echo(`\n`);
+		process.exit(0);
 	}
 	else
 	{
-		shell.echo(`\n${chalk.red(`[ERROR] ${stderr}`)}\n`);
+		spinner.stopAndPersist({
+			symbol: '💀',
+			text: `${chalk.red(stderr)}`
+		});
+		
+		shell.echo(`\n`);
+		shell.exit(1);
 	}
 
 }
 catch(e)
 {
-	shell.echo(`\n${chalk.red('Ruh Roh... Something Went Wrong...')} ${e}\n`);
+	spinner.stopAndPersist({
+		symbol: '💀',
+		text: `${chalk.red(e)}`
+	});
+	
+	shell.echo(`\n`);
+	shell.exit(1);
 }
 
-process.exit(0); //no errors occurred
+process.exit(0);
